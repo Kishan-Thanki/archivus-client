c7c1cd852a6c5aae599af451824e5487b774b64c
Meet Gandhi <meetgandhi4041@gmail.com>
2025-06-20 00:52:20 +0530

    ContactUs-Page

---
+++ src/pages/ContactPage/ContactPage.jsx
@@ -0,0 +1,12 @@
+// src/pages/ContactPage.jsx
+import React from 'react';
+import { Box, Typography } from '@mui/material';
+
+const ContactPage = () => (
+  <Box sx={{ p: 3 }}>
+    <Typography variant="h4">Contact Page</Typography>
+    <Typography>Get in touch with us.</Typography>
+  </Box>
+);
+
+export default ContactPage;
